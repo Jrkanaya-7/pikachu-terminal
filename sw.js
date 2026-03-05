bdbdd2585dfbe3ea00d4ecd341ab9367d9669250
Jrkanaya-7 <clauding1212312@gmail.com>
2026-03-04 20:36:39 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+importScripts("https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/scramjet.all.js");
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sj = new ScramjetServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith((async () => {
+        await sj.loadConfig();
+        if (sj.route(event)) return sj.fetch(event);
+        return fetch(event.request);
+    })());
+});
